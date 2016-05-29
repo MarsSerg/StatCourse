a33b24c429647fac79282877850b8135b663eb39
Sergey <mydadwasmars@yandex.ru>
2016-05-30 01:03:29 +0300

    Revert "WAIT FOR FEATURES"

--- graph.pas
+++ graph.pas
@@ -5,10 +5,9 @@ unit graph;
 interface
 
 uses
-Classes, SysUtils, printers, mssqlconn, Math, sqldb, DB, FileUtil, TADbSource,
-TAGraph, TAStyles, TASources, TANavigation, TASeries, TAMultiSeries, LR_Class,
-LR_Desgn, LR_DBSet, LR_PGrid, LR_Shape, Forms, Controls, Graphics, Dialogs,
-StdCtrls, ExtCtrls, ComCtrls, Grids;
+Classes, SysUtils, mssqlconn, sqldb, DB, FileUtil, TADbSource, TAGraph,
+TAStyles, TASources, TANavigation, TASeries, TAMultiSeries, Forms, Controls,
+Graphics, Dialogs, StdCtrls, ExtCtrls, ComCtrls, Grids;
 
 type
 TTeachers = class
@@ -27,10 +26,7 @@ TForm1 = class(TForm)
   Chart1BarSeries2: TBarSeries;
   Chart2: TChart;
   Chart2BarSeries1: TBarSeries;
-  frDesigner1: TfrDesigner;
-  frReport1: TfrReport;
-  grb: TGroupBox;
-  Image1: TImage;
+  GRB: TGroupBox;
   LableChartSource: TListChartSource;
   Main: TGroupBox;
   Ok: TPanel;
@@ -48,48 +44,12 @@ TForm1 = class(TForm)
   SQLTransaction1: TSQLTransaction;
   StringGrid1: TStringGrid;
   View: TTreeView;
-
-
-  procedure grbMouseDown(Sender: TObject; Button: TMouseButton;
-		    Shift: TShiftState; X, Y: Integer);
-  procedure grbMouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer);
-  procedure grbMouseUp(Sender: TObject; Button: TMouseButton;
-		    Shift: TShiftState; X, Y: Integer);
-  procedure Image1Click(Sender: TObject);
   procedure OkClick(Sender: TObject);
-  procedure Panel2MouseDown(Sender: TObject; Button: TMouseButton;
-		    Shift: TShiftState; X, Y: Integer);
-  procedure Panel2MouseUp(Sender: TObject; Button: TMouseButton;
-		    Shift: TShiftState; X, Y: Integer);
   procedure Panel3Click(Sender: TObject);
-  procedure Panel3MouseDown(Sender: TObject; Button: TMouseButton;
-		    Shift: TShiftState; X, Y: Integer);
-  procedure Panel3MouseUp(Sender: TObject; Button: TMouseButton;
-		    Shift: TShiftState; X, Y: Integer);
   procedure Panel4Click(Sender: TObject);
-  procedure Panel4MouseDown(Sender: TObject; Button: TMouseButton;
-		    Shift: TShiftState; X, Y: Integer);
-  procedure Panel4MouseUp(Sender: TObject; Button: TMouseButton;
-		    Shift: TShiftState; X, Y: Integer);
-
   procedure ViewChange(Sender: TObject; Node: TTreeNode);
   procedure Panel2Click(Sender: TObject);
   procedure ForInfo ;
-  procedure MyResize(NewWidth: integer);
-  procedure FormActivate(Sender: TObject);
-  procedure StringGrid1MouseDown(Sender: TObject; Button: TMouseButton;
-		  Shift: TShiftState; X, Y: Integer);
-  procedure StringGrid1MouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer
-		  );
-  procedure StringGrid1Up(Sender: TObject; Button: TMouseButton;
-		  Shift: TShiftState; X, Y: Integer);
-   procedure Chart1MouseDown(Sender: TObject; Button: TMouseButton;
-		  Shift: TShiftState; X, Y: Integer);
-  procedure Chart1MouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer
-		  );
-  procedure Chart1Up(Sender: TObject; Button: TMouseButton;
-		  Shift: TShiftState; X, Y: Integer);
-  procedure frReport1GetValue(const ParName: String; var ParValue: Variant);
 private
   { private declarations }
 public
@@ -106,46 +66,10 @@ selectedNode: TTreeNode;
 idnode: integer;
 IdAnk, Counted, Summary, Average,pers: integer;
 Sur, forcap,GR: string;
-Draging: Boolean;
-X0, Y0: integer;
 
 {$R *.lfm}
 
 { TForm1 }
-
-
-procedure TForm1.frReport1GetValue(const ParName: String; var ParValue: Variant
-		  );
-var a:string;
-begin
-       a:=inttostr(idank);
-       if ParName = 'count' then ParValue := Counted;
-       if ParName = 'surname' then ParValue := Sur;
-       if ParName = 'idank' then ParValue := a;
-       if ParName = 'average' then ParValue := inttostr(Average);
-       if ParName = 'sum' then ParValue := inttostr(Summary);
-end;
-
-procedure TForm1.MyResize(NewWidth: integer);
-var
-  J: integer;
-  N: integer;
-  S: integer;
-begin
-  N := Abs(NewWidth - Self.Width);
-  S := Sign(NewWidth - Self.Width);
-  for J := 1 to N do
-    begin
-    Self.Width := Self.Width + S;
-    Sleep(0);
-    Application.ProcessMessages;
-    end;
-end;
-
-procedure TForm1.FormActivate(Sender: TObject);
-begin
-  form1.Left := (Screen.Width - Width) div 2;
-end;
 
 procedure TForm1.OkClick(Sender: TObject);
 var
@@ -153,18 +77,7 @@ var
   tree: tteachers;
   l: TTreeNode;
 begin
-  if (Loginedit.Text <> 'Admin') or (PassEdit.text <> '1234') then
-    begin
-    ShowMessage('Пользователя не существует');
-    Loginedit.Text:='';
-    PassEdit.text:='';
-    end
-  else
-
-  try
-    form1.Height:=410;
-    MyResize(90);
-    Main.Visible:=true;
+    try
     SqlQuery1.SQL.Clear;
     SqlQuery1.SQL.Add('SELECT * from Teacher');
     SqlQuery1.Open;
@@ -187,101 +100,6 @@ begin
     end;
 end;
 
-procedure TForm1.Panel2MouseDown(Sender: TObject; Button: TMouseButton;
-		  Shift: TShiftState; X, Y: Integer);
-begin
-                 Panel2.Color := $00A25151;
-end;
-
-procedure TForm1.Panel2MouseUp(Sender: TObject; Button: TMouseButton;
-		  Shift: TShiftState; X, Y: Integer);
-begin
-           Panel2.Color:= $00775100;
-end;
-
- procedure TForm1.Chart1MouseDown(Sender: TObject; Button: TMouseButton;
-		  Shift: TShiftState; X, Y: Integer);
-begin
-    Draging := true;
-  x0 := x;
-  y0 := y;
-end;
-
-procedure TForm1.Chart1MouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer
-		  );
-begin
-                  if Draging = true then
-  begin
-   Chart1.Left := Chart1.Left + X - X0;
-    Chart1.top := Chart1.top + Y - Y0;
-  end;
-end;
-
-procedure TForm1.Chart1Up(Sender: TObject; Button: TMouseButton;
-		  Shift: TShiftState; X, Y: Integer);
-begin
-                   Draging := false;
-end;
-
-
-
-procedure TForm1.StringGrid1MouseDown(Sender: TObject; Button: TMouseButton;
-		  Shift: TShiftState; X, Y: Integer);
-begin
-    Draging := true;
-  x0 := x;
-  y0 := y;
-end;
-
-procedure TForm1.StringGrid1MouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer
-		  );
-begin
-                  if Draging = true then
-  begin
-    StringGrid1.Left := StringGrid1.Left + X - X0;
-    StringGrid1.top := StringGrid1.top + Y - Y0;
-  end;
-end;
-
-procedure TForm1.StringGrid1Up(Sender: TObject; Button: TMouseButton;
-		  Shift: TShiftState; X, Y: Integer);
-begin
-                   Draging := false;
-end;
-
-procedure TForm1.grbMouseDown(Sender: TObject; Button: TMouseButton;
-		  Shift: TShiftState; X, Y: Integer);
-begin
-    Draging := true;
-  x0 := x;
-  y0 := y;
-end;
-
-procedure TForm1.grbMouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer
-		  );
-begin
-                  if Draging = true then
-  begin
-    grb.Left := Grb.Left + X - X0;
-    Grb.top := Grb.top + Y - Y0;
-  end;
-end;
-
-procedure TForm1.grbMouseUp(Sender: TObject; Button: TMouseButton;
-		  Shift: TShiftState; X, Y: Integer);
-begin
-                   Draging := false;
-end;
-
-procedure TForm1.Image1Click(Sender: TObject);
-begin
-
-   frReport1.LoadFromFile('report.lrf');
-   frReport1.ShowReport;
-end;
-
-
-
 procedure TForm1.ForInfo ;
 begin
   SQLQuery1.Sql.Clear;
@@ -301,7 +119,7 @@ begin
   Chart1BarSeries1.Clear;
   Chart1BarSeries1.Add(Average, 'Average', clYellow);
   Chart1BarSeries1.Add(Summary, 'Summary', clGreen);
-  forcap := 'Статистика по ' + IntToStr(Counted) + ' ответам';
+  forcap := 'Статистика по ' + IntToStr(Counted) + ' вопросам';
   panel1.Caption := forcap;
 end;
 
@@ -313,24 +131,8 @@ begin
         chart1.Visible:=true;
 end;
 
-procedure TForm1.Panel3MouseDown(Sender: TObject; Button: TMouseButton;
-		  Shift: TShiftState; X, Y: Integer);
-begin
-                  Panel3.color:= $00A25151;
-end;
-
-procedure TForm1.Panel3MouseUp(Sender: TObject; Button: TMouseButton;
-		  Shift: TShiftState; X, Y: Integer);
-begin
-             Panel3.Color:= $00775100;
-end;
-
 procedure TForm1.Panel4Click(Sender: TObject);
-var
-col:TColor;
-r:byte;
 begin
-  col:=ColorToRGB(clPurple);
     if grb.Visible=true then
       grb.Visible:=false
       else
@@ -339,33 +141,17 @@ begin
   SqlQuery1.SQL.Add('exec Persantage @Id=:IDTEA ');
   SQLQuery1.ParamByName('IdTea').AsInteger := idnode;
   SQLQuery1.Open;
-  Chart2BarSeries1.Clear;
-  while not SQLQuery1.EOF do
+  if not SQLQuery1.EOF then
     begin
-    col:=col+$00EBD2BA;
     GR := SQLQuery1.FieldByName('GroupNumber').AsString;
     Pers := strtoint(SQLQuery1.FieldByName('SatisfactionPercentage').AsString);
-  Chart2BarSeries1.Add(Pers, GR, col);
-  SQLQuery1.Next;
     end;
   SQLTransaction1.Commit;
   SqlQuery1.Close;
+  Chart2BarSeries1.Clear;
+  Chart2BarSeries1.Add(Pers, 'Persantage', clPurple);
 
 end;
-
-procedure TForm1.Panel4MouseDown(Sender: TObject; Button: TMouseButton;
-		  Shift: TShiftState; X, Y: Integer);
-begin
-            Panel4.color:= $00A25151;
-end;
-
-procedure TForm1.Panel4MouseUp(Sender: TObject; Button: TMouseButton;
-		  Shift: TShiftState; X, Y: Integer);
-begin
-                 Panel4.Color:= $00775100;
-end;
-
-
 
 
 
@@ -373,7 +159,7 @@ end;
 procedure TForm1.ViewChange(Sender: TObject; Node: TTreeNode);
 
 begin
-  MyResize(688);
+
   selectedNode := node;
   idnode := TTeachers(node.Data).idTeach;
   forinfo;
@@ -392,22 +178,22 @@ begin
     SQLQuery1.SQL.Clear;
     SQLQuery1.SQL.Add('Exec Allteach ');
     SQLQuery1.Open;
-    StringGrid1.ColCount := 4;
+    StringGrid1.ColCount := 5;
     StringGrid1.RowCount := 1;
-    StringGrid1.Cells[0, 0] := 'Фамилия';
-    StringGrid1.Cells[1, 0] := 'Всего ответов';
-    StringGrid1.Cells[2, 0] := 'Средний балл';
-    StringGrid1.Cells[3, 0] := 'Суммарный балл';
+    StringGrid1.Cells[1, 0] := 'Фамилия';
+    StringGrid1.Cells[2, 0] := 'Всего вопросов';
+    StringGrid1.Cells[3, 0] := 'Средний балл';
+    StringGrid1.Cells[4, 0] := 'Суммарный балл';
     while not SQLQuery1.EOF do
       begin
       StringGrid1.RowCount := StringGrid1.RowCount + 1;
-      StringGrid1.Cells[0, StringGrid1.RowCount - 1] :=
-        SQLQuery1.FieldByName('Surname').AsString;
       StringGrid1.Cells[1, StringGrid1.RowCount - 1] :=
-       SQLQuery1.FieldByName('Counted').AsString;
+        SQLQuery1.FieldByName('Surname').AsString;
       StringGrid1.Cells[2, StringGrid1.RowCount - 1] :=
-        SQLQuery1.FieldByName('Average').AsString;
+       SQLQuery1.FieldByName('Counted').AsString;
       StringGrid1.Cells[3, StringGrid1.RowCount - 1] :=
+        SQLQuery1.FieldByName('Average').AsString;
+      StringGrid1.Cells[4, StringGrid1.RowCount - 1] :=
         SQLQuery1.FieldByName('Summary').AsString;
       SQLQuery1.Next;
       end;
